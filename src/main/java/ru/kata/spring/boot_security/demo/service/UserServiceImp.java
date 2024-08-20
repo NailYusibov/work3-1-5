9988f66d0ec51bfd8d692e46cbc5c2dd2cfb884d
yusib <yusibovnail@gmail.com>
2024-08-20 18:39:16 +0300

    class UserServiceImp
    - зачем инжектишь сюда репозиторий ролей? что бы возвращать роли? Это задача сервиса ролей а не сервиса юзеров. Вырежи метод getAllRoles() из сервиса юзеров и инжект репозитория ролей отсюда
    
    class AdminController
    
    - верни все роли через сервис ролей

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
-
 import java.util.List;
 
 @Service
